0035c4a08238b7840908061897bbff9612d15eb6
tsdimitrov <tsdimitrov@htmlbox.net>
2021-11-10 18:07:06 +0200

    Added link to homepage button

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,8 +1,14 @@
 import { StyledButton, StyledButtonText } from "./elements";
 
+const handleClick = (e) => {
+  e.preventDefault();
+  window.location.href = '/agencySelection';
+
+}
+
 export const Button = ({ children, ...props }) => {
   return (
-    <StyledButton {...props}>
+    <StyledButton {...props} onClick={handleClick}>
       <StyledButtonText>{children}</StyledButtonText>
     </StyledButton>
   );
